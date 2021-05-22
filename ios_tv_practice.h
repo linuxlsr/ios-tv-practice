a13e688b764ee022634587d130bfb2e1bfe78b0a
Charles Harvey <soops@ucla.edu>
2021-05-22 10:48:39 -0700

    initial project framework

---
+++ ios_tv_practice.h
@@ -0,0 +1,18 @@
+//
+//  ios_tv_practice.h
+//  ios-tv-practice
+//
+//  Created by serveradmin on 5/22/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ios_tv_practice.
+FOUNDATION_EXPORT double ios_tv_practiceVersionNumber;
+
+//! Project version string for ios_tv_practice.
+FOUNDATION_EXPORT const unsigned char ios_tv_practiceVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ios_tv_practice/PublicHeader.h>
+
+
